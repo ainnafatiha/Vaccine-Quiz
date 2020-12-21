bb6da0a28847d23f18b91a56bb1ee99924ea53bf
ainnafatiha <ainnafatiha@gmail.com>
2020-12-21 12:14:50 +0800

    commit
    - new questions
    - updated content
    - buttons

--- js/app-config.js
+++ js/app-config.js
@@ -20,12 +20,12 @@ var quizJSON = {
                 {"option": "Myth",      "correct": true},
                 {"option": "Truth",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>That's correct, it is a myth!</span> Infant immune systems are much stronger than they seem. In theory, a baby's body has the ability to respond to about 10,000 vaccines at one time!<br>The immune systems can never fully overpowered as the cells in the human body are always being replenished - babies are exposed to countless bacteria and viruses every day.<br>Even though there are more available now, new vaccines are more efficient and have fewer ingredient than those in previous decades.</p>",
+            "correct": "<p><span>That's correct, it is a myth!</span> The immune system can never fully be overpowered as the cells in the human body are always being replenished - babies are exposed to countless bacteria and viruses every day.<br>Even though there are more available now, new vaccines are more efficient and have fewer ingredients than those in previous decades.</p>",
             
-            "incorrect": "<p><span>Oops, actually the answer is myth.</span> Infant immune systems are much stronger than they seem. In theory, a baby's body has the ability to respond to about 10,000 vaccines at one time!<br>The immune systems can never fully overpowered as the cells in the human body are always being replenished - babies are exposed to countless bacteria and viruses every day.<br>Even though there are more available now, new vaccines are more efficient and have fewer ingredient than those in previous decades.</p>" // no comma here
+            "incorrect": "<p><span>Oops, actually the answer is myth.</span> The immune system can never fully be overpowered as the cells in the human body are always being replenished - babies are exposed to countless bacteria and viruses every day.<br>Even though there are more available now, new vaccines are more efficient and have fewer ingredients than those in previous decades.</p>" // no comma here
         },
         { // Question 2 
-            "q": "Vaccines cause autism.",
+            "q": "Vaccines cause autism",
             "a": [
                 {"option": "Myth",               "correct": true},
                 {"option": "Truth",               "correct": false} // no comma here
@@ -45,7 +45,7 @@ var quizJSON = {
             "incorrect": "<p><span>Oops, actually the answer is myth.</span> In a few cases, becoming sick and recovering naturally results in stronger immunity to the disease than vaccination.<br>However, this approach is far too risky because the dangers outweigh the benefits - for example, if your child contracted measles, he / she would have a 1 in 500 chance of death from the effects of the disease. If you compare this with the number of people who have had severe allergic reactions from the MMR vaccine (less than 1 in one million) vaccinating your child is the much safer option.</p>" // no comma here
         },
         { // Question 4
-            "q": "Vaccines contain unsafe toxins.",
+            "q": "Vaccines contain unsafe toxins",
             "a": [
                 {"option": "Myth",      "correct": true},
                 {"option": "Truth",   "correct": false} // no comma here
@@ -55,7 +55,7 @@ var quizJSON = {
             "incorrect": "<p><span>Oops, actually the answer is myth.</span> Many people are concerned that vaccines contain mercury, aluminum, and formaldehyde, and while it is true that these chemicals are toxic at high levels, only tiny amounts of these chemicals are used in approved vaccines.<br>Formaldehyde is actually produced at higher rates by our own metabolic systems, and there is no evidence that shows that low levels of any of these chemicals are harmful. </p></p>" // no comma here
         },
         { // Question 5
-            "q": "Vaccines have eradicated diseases globally.",
+            "q": "Vaccines have eradicated diseases globally",
             "a": [
                 {"option": "Myth",      "correct": false},
                 {"option": "Truth",   "correct": true} // no comma here
@@ -65,34 +65,44 @@ var quizJSON = {
             "incorrect": "<p><span>Oops, actually the answer is truth.</span> Many people are concerned that vaccines contain mercury, aluminum, and formaldehyde, and while it is true that these chemicals are toxic at high levels, only tiny amounts of these chemicals are used in approved vaccines.<br>Formaldehyde is actually produced at higher rates by our own metabolic systems, and there is no evidence that shows that low levels of any of these chemicals are harmful. </p></p>" // no comma here
         },
         { // Question 6
-            "q": "Vaccines can infect my child with the disease that it is trying to prevent.",
+            "q": "Vaccines can infect me with the disease that it is trying to prevent",
             "a": [
                 {"option": "Myth",      "correct": true},
                 {"option": "Truth",   "correct": false} // no comma here
             ],
-            "correct": "<p><span>That's correct, it is a myth!</span>Vaccines may cause mild symptoms, which drives the false idea that the child has the disease after vaccination.<br> A small percentage (less than 1 in one million cases) where symptoms do occur, the child is experiencing the body’s immune response to the vaccine, not the disease.<br>Vaccines cause the immune system to produce an immune response similar to the disease, but they do not cause the disease, nor does it put the person at risk of its potential complications.<br>Vaccines have been safe to use for decades and follow strict clinical and regulatory guidelines.</p>",
+            "correct": "<p><span>That's correct, it is a myth!</span>Vaccines may cause mild symptoms, which drives the false idea that you have the disease after vaccination.<br> Vaccines cause the immune system to produce an immune response similar to the disease, but they do not cause the disease, nor does it put the person at risk of its potential complications.<br>Vaccines have been safe to use for decades and follow strict and regulatory guidelines.</p>",
             
-            "incorrect": "<p><span>Oops, actually the answer is myth.</span> Vaccines may cause mild symptoms, which drives the false idea that the child has the disease after vaccination.<br> A small percentage (less than 1 in one million cases) where symptoms do occur, the child is experiencing the body’s immune response to the vaccine, not the disease.<br>Vaccines cause the immune system to produce an immune response similar to the disease, but they do not cause the disease, nor does it put the person at risk of its potential complications.<br>Vaccines have been safe to use for decades and follow strict clinical and regulatory guidelines. </p></p>" // no comma here
+            "incorrect": "<p><span>Oops, actually the answer is myth.</span> Vaccines may cause mild symptoms, which drives the false idea that you have the disease after vaccination.<br> Vaccines cause the immune system to produce an immune response similar to the disease, but they do not cause the disease, nor does it put the person at risk of its potential complications.<br>Vaccines have been safe to use for decades and follow strict and regulatory guidelines.</p>" // no comma here
         },
         { // Question 7
-            "q": "I need to vaccinate my child even if all the other children around him / her are already immunized",
+            "q": "I should vaccinate even if everyone around me is already vaccinated",
             "a": [
                 {"option": "Myth",      "correct": false},
                 {"option": "Truth",   "correct": true} // no comma here
             ],
-            "correct": "<p><span>That's correct, it is a truth!</span> Herd immunity occurs when a large population of the community is immunized against the disease, which reduces the chance of an outbreak.<br>Infants, pregnant women and people with weaker immune systems can’t receive vaccines and depend on this form of protection.<br>If too many people rely on herd immunity to prevent infection from vaccine-preventable disease, herd immunity will disappear</p>",
+            "correct": "<p><span>That's correct, it is a truth!</span> When a large amount of community is vaccinated against a disease, it reduces the change of an outbreak ('herd immunity').<br>Infants, pregnant women and people with wekaer immune systems can't receive vaccines and depend on this form of protection.<br>If too many people rely on herd immunity to prevent infection from vaccine-preventable disease, herd immunity will disappear.</p>",
             
-            "incorrect": "<p><span>Oops, actually the answer is truth.</span> Herd immunity occurs when a large population of the community is immunized against the disease, which reduces the chance of an outbreak.<br>Infants, pregnant women and people with weaker immune systems can’t receive vaccines and depend on this form of protection.<br>If too many people rely on herd immunity to prevent infection from vaccine-preventable disease, herd immunity will disappear </p></p>" // no comma here
+            "incorrect": "<p><span>Oops, actually the answer is truth.</span> When a large amount of community is vaccinated against a disease, it reduces the change of an outbreak ('herd immunity').<br>Infants, pregnant women and people with wekaer immune systems can't receive vaccines and depend on this form of protection.<br>If too many people rely on herd immunity to prevent infection from vaccine-preventable disease, herd immunity will disappear.</p>" // no comma here
         },
         { // Question 8
-            "q": "I can vaccinate my child later in life",
+            "q": "Healthy adults still need vaccines",
+            "a": [
+                {"option": "Myth",      "correct": false},
+                {"option": "Truth",   "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's correct, it is a truth!</span> Even if you are healthy, your immune system will weaken as you continue to age.<br>At the ageof 65 or above, adults are at an 8x higher risk of being hospitalized due to infection when compared to those who are younger than 50.</p>",
+            
+            "incorrect": "<p><span>Oops, actually the answer is truth.</span> Even if you are healthy, your immune system will weaken as you continue to age.<br>At the ageof 65 or above, adults are at an 8x higher risk of being hospitalized due to infection when compared to those who are younger than 50.</p>" // no comma here
+        },
+        { // Question 9
+            "q": "Vaccines are not effective in adults",
             "a": [
                 {"option": "Myth",    "correct": true},
                 {"option": "Truth",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>That's correct, it is a myth!</span> Any delay in vaccinating your child can leave him / her at risk of the vaccine-preventable disease.<br>The best way to ensure your child’s safety is to follow the guidelines and schedule set out by the Ministry of Health (please click<b>HERE<b>), in consultation with your doctor.</p>",
+            "correct": "<p><span>That's correct, it is a myth!</span> Any delay in vaccinating your child can leave him / her at risk of the vaccine-preventable disease. However, it is never too late as vaccines are still effective for adults. With your increased responsibilities as an adult, you should get vaccinated - protect those around you from getting the disease and stay healthy enough to work and take care of your family.<br>The best way to ensure you and your family's safety is to follow the guidelines and schedule set out by the Ministry of Health, in consultation with your doctor.</p>",
             
-            "incorrect": "<p><span>Oops, actually the answer is myth.</span> Any delay in vaccinating your child can leave him / her at risk of the vaccine-preventable disease.<br>The best way to ensure your child’s safety is to follow the guidelines and schedule set out by the Ministry of Health (please click<b>HERE<b>), in consultation with your doctor.</p>" // no comma here
+            "incorrect": "<p><span>Oops, actually the answer is myth.</span> Any delay in vaccinating your child can leave him / her at risk of the vaccine-preventable disease. However, it is never too late as vaccines are still effective for adults. With your increased responsibilities as an adult, you should get vaccinated - protect those around you from getting the disease and stay healthy enough to work and take care of your family.<br>The best way to ensure you and your family's safety is to follow the guidelines and schedule set out by the Ministry of Health, in consultation with your doctor.</p>" // no comma here
         } // no comma here
     ]
 };
